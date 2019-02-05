415d39d6ffb768c38e00a84698f22f8c6fcdb21a
Edoardo Canti <edoardo.canti.mail@gmail.com>
2019-02-05 19:38:25 +0100

    Started directed graph

--- main.cpp
+++ main.cpp
@@ -48,9 +48,9 @@ int main()
     
     //Aggiunita di un nuovo nodo e di due nuovi archi a partire da un grafo già costruito
     
-    boost::adjacency_list<>::vertex_descriptor v1 = boost::add_vertex(graph);
-    boost::add_edge(0, v1, graph);
-    boost::add_edge(4, v1, graph);
+    boost::adjacency_list<>::vertex_descriptor v = boost::add_vertex(graph);
+    boost::add_edge(0, v, graph);
+    boost::add_edge(4, v, graph);
     
     std::cout<<std::endl<<std::endl;
     
@@ -87,6 +87,37 @@ int main()
             std::cout<<"In-Edge(): "<<*inEdge<<" "<<std::endl;
     }
     
+    std::cout<<std::endl;
+    
+    std::cout<<"[ DIRECTED GRAPH ]"<<std::endl;
+    
+    /*
+     directG structure: 2 <--> 0 --> 1
+     */
+    
+    //definisco un tipo di grafo diretto (orientato)
+    typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS> directed_graph;
+    directed_graph directG; //definizione del tipo di grafo diretto (orientato)
+    
+    directed_graph::vertex_iterator d_iterator, d_iteratorEnd; //iteratore vertici (grafo diretto)
+    directed_graph::adjacency_iterator d_adjNode, d_adjNodeEnd; //iteratore adiacenze (grafo diretto)
+    directed_graph::in_edge_iterator d_inEdge, d_inEdgeEnd; //iteratore in-edges
+    directed_graph::out_edge_iterator d_outEdge, d_outEdgeEnd; //iteratore out-edges
+    
+    boost::adjacency_list<>::vertex_descriptor v1 = boost::add_vertex(directG); //descrittore nodo1
+    boost::adjacency_list<>::vertex_descriptor v2 = boost::add_vertex(directG); //descrittore nodo2
+    boost::adjacency_list<>::vertex_descriptor v3 = boost::add_vertex(directG); //descrittore nodo3
+    
+    boost::add_edge(v1, v2, directG); //aggiunta arco(v1,v2)
+    boost::add_edge(v1, v3, directG); //aggiunta arco(v1,v3)
+    boost::add_edge(v3, v1, directG); //aggiunta arco (v3,v1)
+    
+    std::cout<<"Edge-List: "<<std::endl;
+    for(boost::tie(d_iterator, d_iteratorEnd) = boost::vertices(directG); d_iterator!=d_iteratorEnd; ++d_iterator)
+    {
+        for(boost::tie(d_adjNode, d_adjNodeEnd)=boost::adjacent_vertices(*d_iterator, directG); d_adjNode!=d_adjNodeEnd; ++d_adjNode)
+            std::cout<<"Edge("<<*d_iterator<<","<<*d_adjNode<<")"<<std::endl;
+    }
     
 }
 
